9227525a34ea45638f25cd0f92860aa150e229d2
Christoph Dorn <cadorn@christoph-dorns-macbook-pro-17.local>
2009-10-01 19:03:57 -0700

    minor fixes

--- packages/common/lib/common/build/dev.js
+++ packages/common/lib/common/build/dev.js
@@ -6,6 +6,7 @@ var FILE = require('file');
 var UTIL = require("util");
 var STREAM = require('term').stream;
 var SEA = require("narwhal/tusk/sea");
+var PACKAGES = require("packages");
 
 var HARNESS = require("./harness");
 
@@ -14,7 +15,7 @@ var HARNESS = require("./harness");
 exports.main = function(args, options) { with(HARNESS.initialize(args, options)) {
     
 
-    // link narwhal directory
+    // link narwhal
     
     fromPath = FILE.Path(system.prefix);
     toPath = targetBuildChromePath.join("narwhalrunner", "narwhal");
@@ -23,6 +24,39 @@ exports.main = function(args, options) { with(HARNESS.initialize(args, options))
         fromPath.symlink(toPath);
     }
     print("Linked '" + toPath + "' to '" + fromPath + "'");    
+
+
+    // link narwhal-xulrunner engine
+/*    
+    NOTE: For now we assume it is in narwhal/engines/xulrunner
+    
+    var xulrunnerEnginePath,
+        scanPackages = [
+            "narwhal/narwhal-xulrunner/link",   // This will be deprecated soon
+            "narwhal/narwhal-xulrunner/latest",
+            "narwhal-xulrunner"
+        ];
+    
+    scanPackages.forEach(function(name) {
+        if(xulrunnerEnginePath) {
+            return xulrunnerEnginePath;
+        }
+        if(UTIL.has(PACKAGES.catalog, name)) {
+            xulrunnerEnginePath = PACKAGES.catalog[name].directory;
+        }    
+    })
+    if(!xulrunnerEnginePath) {    
+        throw "Cannot find narwhal-xulrunner engine!";
+    }
+    
+    fromPath = xulrunnerEnginePath;
+    toPath = targetBuildPath.join("packages", "narwhal-xulrunner");
+    if(!toPath.exists()) {
+        toPath.dirname().mkdirs();    
+        fromPath.symlink(toPath);
+    }
+    print("Linked '" + toPath + "' to '" + fromPath + "'");    
+*/    
 
 
     // link chrome/content
